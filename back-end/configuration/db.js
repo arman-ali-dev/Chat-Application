d4003cbb60d47bdceebdc262fa7c9be01826c0c6
Arman ali dev <armaanali.dev@gmail.com>
2025-01-23 12:13:50 +0530

    m

--- back-end/configuration/db.js
+++ back-end/configuration/db.js
@@ -1,9 +1,7 @@
 const { connect } = require("mongoose");
 
 const connectMongoDB = async (URI) => {
-  const conn = await connect(
-    "mongodb+srv://armaanalidev:arman5911@chat-app.wwnnl.mongodb.net/Chat-App"
-  );
+  const conn = await connect(URI);
   return conn;
 };
 

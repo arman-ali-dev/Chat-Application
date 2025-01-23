818bbd1a806a9ea8f429088672b51b01913e0d0a
Arman ali dev <armaanali.dev@gmail.com>
2025-01-23 12:07:09 +0530

    change the uri

--- back-end/configuration/db.js
+++ back-end/configuration/db.js
@@ -2,7 +2,7 @@ const { connect } = require("mongoose");
 
 const connectMongoDB = async (URI) => {
   const conn = await connect(
-    "mongodb+srv://armaanalidev:arman5911@chat-app.wwnnl.mongodb.net/CHAT_APP"
+    "mongodb+srv://armaanalidev:arman5911@chat-app.wwnnl.mongodb.net/Chat-App"
   );
   return conn;
 };
